cfc67ab439f63659010b648491b0862a4777d4cd
kittyperson <karenvanyan1994@gmail.com>
2020-12-02 23:47:55 +0800

    init cimmit

---
+++ data/proposals.js
@@ -0,0 +1,16 @@
+
+let proposals = [
+    {
+        id: 1,
+        name: {
+            zh:'提案1',
+            en:'Proposal 1'
+        },
+        description: {
+            zh:'这是提案1的介绍',
+            en:'This is the description of Proposal 1'
+        }
+    }
+]
+
+module.exports = proposals;
